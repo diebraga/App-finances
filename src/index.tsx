4230efb0e9245d78f7fc712320f3493f9609b9f2
Diebraga <diebraga0055@gmail.com>
2021-04-12 00:40:23 +0100

    db fake

--- src/index.tsx
+++ src/index.tsx
@@ -2,26 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freela webpage',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 4500,
+          createdAt: new Date('2021-03-15 08:00:00')
+        },
+
+        {
+          id: 2,
+          title: 'Supermercado',
+          type: 'withdraw',
+          category: 'Compras',
+          amount: 1200,
+          createdAt: new Date('2021-03-15 13:00:00')
+        }
+      ]
+    });
+  },
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
-    })
+      return this.schema.all('transaction')
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create('transaction', data);
+    });
   }
-})
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
